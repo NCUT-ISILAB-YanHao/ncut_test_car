740471b4ccb038ef9b6ff7d97d4e72f12ae7a656
NCUT-Yan-Hao <yanhao95106@gm.student.ncut.edu.tw>
2023-01-01 18:51:42 +0800

    first commit

---
+++ mainwindow.h
@@ -0,0 +1,46 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+protected:
+    void keyPressEvent(QKeyEvent *);
+
+
+private slots:
+    void update_time();//遊戲時間更新
+    void update_object();//遊戲物體移動更新
+
+    void on_button_start_game_clicked();
+
+private:
+    Ui::MainWindow *ui;
+
+    int time; //遊戲時間
+    int game_status; //遊戲狀態
+    int bgm_pos; //背景的位置(水平)
+    int car_pos; //車子對位置(垂直)
+    int car_direction; //車子的方向(垂直)
+    float car_distance; //車子移動的距離
+
+    QTimer *clock_timer; //遊戲時間計時器
+    QTimer *object_timer; //物體移動計時器
+
+    void game_start();
+    void game_pause();
+    void game_stop();
+    void move_car();
+};
+#endif // MAINWINDOW_H
